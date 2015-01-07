df53cd7a6aa7d6f00916aede539f56497c4749e3
Austin <austin.goudge@psycle.com>
2015-01-07 16:08:46 +0000

    More WIP on regions

--- src/Psycle/SJTMapTools/Region.php
+++ src/Psycle/SJTMapTools/Region.php
@@ -2,6 +2,11 @@
 
 namespace Psycle\SJTMapTools;
 
+use pocketmine\block\Gold;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\Server;
+
 /**
  * A container for a region of data in the map
  */
@@ -21,7 +26,7 @@ class Region {
      * @var type string
      */
     private $dataFolder;
-    
+
     function __construct($name, $userName, $x1, $y1, $z1, $x2, $y2, $z2, $dataFolder) {
         print('Created region: ' . $name . ' ' . $userName . ' ' .  $x1 . ' ' .  $y1 . ' ' .  $z1 . ' ' .  $x2 . ' ' .  $y2 . ' ' .  $z2);
         $this->name = $name;
@@ -34,11 +39,11 @@ class Region {
         $this->dataFolder = $dataFolder;
         // TODO store initial data in object
     }
-    
+
     /**
-     * Write our region data to disk.  This writes both region metadata and 
+     * Write our region data to disk.  This writes both region metadata and
      * the Minecraft data from the map.
-     * 
+     *
      * @param boolean $createRevision If true, also push to Git
      * @return boolean true if successful
      */
@@ -47,16 +52,31 @@ class Region {
         // TODO write region Minecraft data
         return true;
     }
-    
+
     /**
-     * Read our region data from disk.  This reads the region metadata and 
+     * Read our region data from disk.  This reads the region metadata and
      * the Minecraft data into the map.
-     * 
+     *
      * @return boolean true if successful
      */
     public function read() {
         // TODO read region metadata
         // TODO read region Minecraft data
         return true;
+    }
+
+    /**
+     * Draw marker blocks at the limits of the region
+     */
+    public function drawMarkers() {
+        $level = Server::getInstance()->getDefaultLevel();
+        $level->setBlockIdAt((int)$this->x1, (int)$this->y1 + 1, (int)$this->z1, 1);
+        $level->setBlock(new Vector3($this->x2, $this->y1, $this->z1), new Gold(), true);
+        $level->setBlock(new Vector3($this->x1, $this->y2, $this->z1), new Gold(), true);
+        $level->setBlock(new Vector3($this->x2, $this->y2, $this->z1), new Gold(), true);
+        $level->setBlock(new Vector3($this->x1, $this->y1, $this->z2), new Gold(), true);
+        $level->setBlock(new Vector3($this->x2, $this->y1, $this->z2), new Gold(), true);
+        $level->setBlock(new Vector3($this->x1, $this->y2, $this->z2), new Gold(), true);
+        $level->setBlock(new Vector3($this->x2, $this->y2, $this->z2), new Gold(), true);
     }
 }
--- src/Psycle/SJTMapTools/RegionManager.php
+++ src/Psycle/SJTMapTools/RegionManager.php
@@ -2,9 +2,6 @@
 
 namespace Psycle\SJTMapTools;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-
 
 /**
  * Manages editable regions or a world.  Allows allocation of a permit to edit a
@@ -22,16 +19,17 @@ class RegionManager {
      */
     private $dataFolder;
     /**
-     * An associative array of all Region objects
+     * An associative array of all Region objects. key=regionname, value=Region
      * @var type array of Regions
      */
     private $regions = array();
     /**
-     * An associative array containing all regions that are in the process of being defined by users. key=username, value=array of start coordinates
+     * An associative array containing all regions that are in the process of
+     * being defined by users. key=username, value=array of start coordinates
      * @var type array
      */
     private $underway = array();
-    
+
     /**
      *  Error codes for return values
      */
@@ -40,17 +38,17 @@ class RegionManager {
           ERROR_REGION_EXISTS = -2,
           ERROR_REGION_ALREADY_STARTED = -3,
           ERROR_REGION_NOT_STARTED = -4;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param string $dataFolder The regions data folder
      */
     function __construct($dataFolder) {
         $this->dataFolder = $dataFolder;
         $this->parseDataFolder();
     }
-    
+
     /**
      * Parse the contents of the data folder.  Loads all found regions.
      */
@@ -59,13 +57,13 @@ class RegionManager {
             SJTMapTools::$instance->getLogger()->info('The regions folder doesn\'t exist.  Cloning from ' . self::$gitRepo . '…');
             GitTools::gitClone($this->dataFolder, self::$gitRepo);
         }
-        
+
         // TODO load all regions
     }
 
     /**
      * Start defining a region for a user
-     * 
+     *
      * @param string $userName The user's name
      * @param int $x The x coordinate of the region start
      * @param int $y The y coordinate of the region start
@@ -76,14 +74,14 @@ class RegionManager {
         if (array_key_exists($userName, $this->underway)) {
             return self::ERROR_REGION_ALREADY_STARTED;
         }
-        
+
         $this->underway[$userName] = [$x, $y, $z];
         return self::NO_ERROR;
     }
-    
+
     /**
      * Cancel the definition of a region
-     * 
+     *
      * @param string $userName The user's name
      * @return int NO_ERROR or an error code
      */
@@ -91,14 +89,14 @@ class RegionManager {
         if (!array_key_exists($userName, $this->underway)) {
             return self::ERROR_REGION_NOT_STARTED;
         }
-        
+
         unset($this->underway[$userName]);
         return self::NO_ERROR;
     }
-    
+
     /**
      * Finish defining a region for a user
-     * 
+     *
      * @param string $userName The user's name
      * @param string $regionName The region's name
      * @param int $x The x coordinate of the region start
@@ -116,12 +114,15 @@ class RegionManager {
 
         $startData = $this->underway[$userName];
         unset($this->underway[$userName]);
-        
-        $this->regions[$regionName] = new Region($regionName, $userName, $startData[0], $startData[1], $startData[2], $x, $y, $z, $this->dataFolder);
-        $this->regions[$regionName]->write(true);
+
+        $region = new Region($regionName, $userName, $startData[0], $startData[1], $startData[2], $x, $y, $z, $this->dataFolder);
+        $this->regions[$regionName] = $region;
+        $region->write(true);
+        $region->drawMarkers();
+
         return self::NO_ERROR;
     }
-    
+
     public function loadRegion($regionName) {
         return null;
     }
